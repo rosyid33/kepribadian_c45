36d91ca133011f539c3bd0515dade2c71b5fea21
rosyid_ido <rosyid33@gmail.com>
2017-05-20 07:08:46 +0700

    master data siswa dan displah pohon keputusan

--- data_siswa.php
+++ data_siswa.php
@@ -1,151 +1,170 @@
 <?php
 //session_start();
-if (!isset($_SESSION['kepribadian_c45_id'])) {
+if (!isset($_SESSION['kepribadian_naive_bayes_id'])) {
     header("location:index.php?menu=forbidden");
 }
 
 include_once "database.php";
 include_once "fungsi.php";
-include_once "import/excel_reader2.php";
+//include_once "import/excel_reader2.php";
 ?>
-<div class="content-wrapper">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4 class="page-head-line">Data Latih</h4>
-            </div>
-        </div>
-        <?php
-        //object database class
-        $db_object = new database();
-
-        $pesan_error = $pesan_success = "";
-        if (isset($_GET['pesan_error'])) {
-            $pesan_error = $_GET['pesan_error'];
-        }
-        if (isset($_GET['pesan_success'])) {
-            $pesan_success = $_GET['pesan_success'];
-        }
-
-        if (isset($_POST['submit'])) {
-            // if(!$input_error){
-            $data = new Spreadsheet_Excel_Reader($_FILES['file_data_latih']['tmp_name']);
-
-            $baris = $data->rowcount($sheet_index = 0);
-            $column = $data->colcount($sheet_index = 0);
-            //import data excel dari baris kedua, karena baris pertama adalah nama kolom
-            // $temp_date = $temp_produk = "";
-            for ($i=2; $i<=$baris; $i++) {
-//                for($c=1; $c<=$column; $c++){
-//                    $value[$c] = $data->val($i, $c);
-//                }
-                $value = "(\"".$data->val($i, 2)."\", '".$data->val($i, 3)."', "
-                        .$data->val($i, 4).", '".$data->val($i, 5)."', "
-                        .$data->val($i, 6).", ".$data->val($i, 7).", "
-                        .$data->val($i, 8).", ".$data->val($i, 9).", '".$data->val($i, 10)."')";
-                $sql = "INSERT INTO data_latih "
-                    . " (nama, jenis_kelamin, usia, sekolah, jawaban_a, jawaban_b, jawaban_c, jawaban_d, kelas_asli)"
-                    . " VALUES ".$value;
-                $result = $db_object->db_query($sql);
-            }
-            //$values = implode(",", $value);
-            
-            if($result){
-                ?>
-                <script> location.replace("?menu=data_latih&pesan_success=Data berhasil disimpan");</script>
-                <?php
-            }
-            else{
-                ?>
-                <script> location.replace("?menu=data_latih&pesan_error=Data gagal disimpan");</script>
-                <?php
-            }
-        }
-
-        if (isset($_POST['delete'])) {
-            $sql = "TRUNCATE data_latih";
-            $db_object->db_query($sql);
-            ?>
-            <script> location.replace("?menu=data_latih&pesan_success=Data latih berhasil dihapus");</script>
+<div class="content">
+    <!--typography-page -->
+    <div class="typo-w3">
+        <div class="container">
+            <h2 class="tittle">Data Siswa</h2>
             <?php
-        }
+            //object database class
+            $db_object = new database();
 
-        $sql = "SELECT * FROM data_latih";
-        $query = $db_object->db_query($sql);
-        $jumlah = $db_object->db_num_rows($query);
-        ?>
+            $pesan_error = $pesan_success = "";
+            if (isset($_GET['pesan_error'])) {
+                $pesan_error = $_GET['pesan_error'];
+            }
+            if (isset($_GET['pesan_success'])) {
+                $pesan_success = $_GET['pesan_success'];
+            }
 
-        <div class="row">
-            <div class="col-md-12">
-                <!--UPLOAD EXCEL FORM-->
-                <form method="post" enctype="multipart/form-data" action="">
-                    <div class="form-group">
-                        <div class="input-group">
-                            <label>Import data from excel</label>
-                            <input name="file_data_latih" type="file" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <input name="submit" type="submit" value="Upload Data" class="btn btn-success">
-                    
-                        <button name="delete" type="submit"  class="btn btn-danger" onclick="">
-                            <i class="fa fa-trash-o"></i> Delete All Data Latih
-                        </button>
-                    </div>
-                </form>
+            if (isset($_POST['submit'])) {
+                $sql1 = "INSERT INTO users "
+                        . " (nama, username, password, level)"
+                        . " VALUES (\"" . $_POST['nama'] . "\", \"" . $_POST['user_name'] . "\", md5(\"" . $_POST['user_name'] . "\"), 2)";
+                $result1 = $db_object->db_query($sql1);
+                $id_usernya = $db_object->db_insert_id();
 
-                <?php
-                if (!empty($pesan_error)) {
-                    display_error($pesan_error);
-                }
-                if (!empty($pesan_success)) {
-                    display_success($pesan_success);
-                }
+                $sql = "INSERT INTO data_siswa "
+                        . " (nama_siswa, jenis_kelamin, usia, sekolah, id_user)"
+                        . " VALUES "
+                        . " (\"" . $_POST['nama'] . "\", \"" . $_POST['jenis_kelamin'] . "\", \"" . $_POST['usia'] . "\","
+                        . " \"" . $_POST['sekolah'] . "\", $id_usernya)";
+                $result = $db_object->db_query($sql);
 
 
-                echo "Jumlah data: " . $jumlah . "<br>";
-                if ($jumlah == 0) {
-                    echo "Data kosong...";
-                } 
-                else {
+                if ($result && $result1) {
                     ?>
-                    <table class='table table-bordered table-striped  table-hover'>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama</th>
-                            <th>Jenis Kelamin</th>
-                            <th>Usia</th>
-                            <th>Sekolah</th>
-                            <th>Jawaban A</th>
-                            <th>Jawaban B</th>
-                            <th>Jawaban C</th>
-                            <th>Jawaban D</th>
-                            <th>Kelas Asli</th>
-                        </tr>
-                        <?php
-                        $no = 1;
-                        while ($row = $db_object->db_fetch_array($query)) {
-                            echo "<tr>";
-                            echo "<td>" . $no . "</td>";
-                            echo "<td>" . $row['nama'] . "</td>";
-                            echo "<td>" . $row['jenis_kelamin'] . "</td>";
-                            echo "<td>" . $row['usia'] . "</td>";
-                            echo "<td>" . $row['sekolah'] . "</td>";
-                            echo "<td>" . $row['jawaban_a'] . "</td>";
-                            echo "<td>" . $row['jawaban_b'] . "</td>";
-                            echo "<td>" . $row['jawaban_c'] . "</td>";
-                            echo "<td>" . $row['jawaban_d'] . "</td>";
-                            echo "<td>" . $row['kelas_asli'] . "</td>";
-                            echo "</tr>";
-                            $no++;
-                        }
-                        ?>
-                    </table>
+                    <script> location.replace("?menu=data_siswa&pesan_success=Data berhasil disimpan");</script>
+                    <?php
+                } else {
+                    ?>
+                    <script> location.replace("?menu=data_siswa&pesan_error=Data gagal disimpan");</script>
                     <?php
                 }
+            }
+
+            if (isset($_GET['delete'])) {
+                $id_delete = $_GET['delete'];
+                $id_usere = get_id_user_siswa($db_object, $id_delete);
+                $sql = "DELETE FROM data_siswa WHERE id=" . $id_delete;
+                $db_object->db_query($sql);
+
+                $sql = "DELETE FROM users WHERE id_user=" . $id_usere;
+                $db_object->db_query($sql);
                 ?>
+                <script> location.replace("?menu=data_siswa&pesan_success=Data siswa berhasil dihapus");</script>
+                <?php
+            }
+
+            $sql = "SELECT siswa.*, usr.username FROM data_siswa siswa, users usr
+                WHERE siswa.`id_user` = usr.`id_user`";
+            $query = $db_object->db_query($sql);
+            $jumlah = $db_object->db_num_rows($query);
+            ?>
+
+            <div class="row">
+                <div class="col-md-12">
+
+                    <form method="post" action="">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <label>Nama</label>
+                                <input name="nama" type="text" class="form-control" required="">
+                            </div>
+                            <div class="input-group">
+                                <label>Username</label>
+                                <input name="user_name" type="text" class="form-control" required="">
+                            </div>
+                            <div class="input-group">
+                                <label>Jenis Kelamin</label>
+                                <div class="radio">
+                                    <label>
+                                        <input name="jenis_kelamin" type="radio" value="L" required=""> Laki-laki
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input name="jenis_kelamin" type="radio" value="P" required=""> Perempuan
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="input-group">
+                                <label>Usia</label>
+                                <input name="usia" type="text" class="form-control" required="">
+                            </div>
+                            <div class="input-group">
+                                <label>Sekolah</label>
+                                <div class="radio">
+                                    <label>
+                                        <input name="sekolah" type="radio" value="Negeri" required=""> Negeri
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input name="sekolah" type="radio" value="Swasta" required=""> Swasta
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input name="submit" type="submit" value="Save" class="btn btn-success">
+                        </div>
+                    </form>
+
+                    <?php
+                    if (!empty($pesan_error)) {
+                        display_error($pesan_error);
+                    }
+                    if (!empty($pesan_success)) {
+                        display_success($pesan_success);
+                    }
+
+
+                    echo "Jumlah data: " . $jumlah . "<br>";
+                    if ($jumlah == 0) {
+                        echo "Data kosong...";
+                    } else {
+                        ?>
+                        <table class='table table-bordered table-striped  table-hover'>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Jenis Kelamin</th>
+                                <th>Usia</th>
+                                <th>Sekolah</th>
+                                <th>Username</th>
+                                <th></th>
+                            </tr>
+    <?php
+    $no = 1;
+    while ($row = $db_object->db_fetch_array($query)) {
+        echo "<tr>";
+        echo "<td>" . $no . "</td>";
+        echo "<td>" . $row['nama_siswa'] . "</td>";
+        echo "<td>" . $row['jenis_kelamin'] . "</td>";
+        echo "<td>" . $row['usia'] . "</td>";
+        echo "<td>" . $row['sekolah'] . "</td>";
+        echo "<td>" . $row['username'] . "</td>";
+        echo "<td><a href='?menu=data_siswa&delete=" . $row['id'] . "'>"
+        . "<img src='images/icon/delete.gif'/></a></td>";
+        echo "</tr>";
+        $no++;
+    }
+    ?>
+                        </table>
+                            <?php
+                        }
+                        ?>
+                </div>
             </div>
         </div>
     </div>
 </div>
-
---
+++ pohon_keputusan.php
@@ -0,0 +1,172 @@
+<?php
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING | E_DEPRECATED));
+include_once "database.php";
+include_once "fungsi.php";
+//object database class
+$db_object = new database();
+?>
+<div class="content">
+    <!--typography-page -->
+    <div class="typo-w3">
+        <div class="container">
+            <h2 class="tittle">Pohon Keputusan</h2>
+
+<?php
+
+if(isset($_GET['act'])){
+    $action=$_GET['act'];
+    $id=$_GET['id'];
+    if($action=='delete'){
+        $db_object->db_query("TRUNCATE t_keputusan");
+        header('location:index.php?menu=pohon_keputusan');
+    }
+}
+
+
+$query=$db_object->db_query("select * from t_keputusan order by(id)");
+$jumlah=$db_object->db_num_rows($query);
+//jika pohon keputusan kosong
+if($jumlah==0){
+    echo "<center><h3> Pohon keputusan belum terbentuk...</h3></center>";
+}
+else{
+    //hanya kaprodi yang bisa menghapus pohon keputusan dan menguji akurasi
+    if($_SESSION['gizibalita_c45_level']==1){
+?>
+        <p>
+            <a href="?menu=pohon_keputusan&act=delete" class="btn btn-danger" onClick="return confirm('Anda yakin akan hapus pohon keputusan?')">
+                Hapus Pohon Keputusan
+            </a>
+            <!--<a href="?menu=pohon_tree" >Lihat Pohon Keputusan</a> |-->
+            <a href="?menu=uji_rule" class="btn btn-default">Uji Rule</a>
+        </p>
+    <?php
+    }
+    echo "Jumlah rule : ".$jumlah."<br>";
+    ?>
+        <table class='table table-bordered table-striped  table-hover'>
+            <tr align='center'>
+                <th>Id</th><th>Aturan</th>
+            </tr>
+            <?php
+                $warna1 = '#ffc';
+                $warna2 = '#eea';
+                $warna  = $warna1;
+                $no=1;
+                while($row=$db_object->db_fetch_array($query)){
+                ?>
+                    <tr>
+                        <td align='center'><?php echo $row['id'];?></td>
+                        <td><?php
+                                echo "IF ";
+                                if($row['parent']!=''){
+                                        echo $row['parent']." AND ";
+                                }
+                                echo $row['akar']." THEN Label = ".$row['keputusan'];?>
+                        </td>
+                    </tr>
+                <?php
+                    $no++;
+                }
+            ?>
+        </table>
+<?php
+}
+
+
+/*
+//select id dari pohon keputusan
+$que_sql = $db_object->db_query("SELECT id FROM t_keputusan");
+$id = array();
+$l = 0;
+while ($bar_row = $db_object->db_fetch_array($que_sql)) {
+    $id[$l] = $bar_row[0];
+    $l++;
+}
+
+$query = $db_object->db_query("SELECT * FROM t_keputusan ORDER BY(id)");
+$temp_rule = array();
+$temp_rule[0] = '';
+$ll = 0; //variabel untuk iterasi id pohon keputusan
+while ($bar = $db_object->db_fetch_array($query)) {
+    //menampung rule
+    if ($bar[1] != '') {
+        $rule = $bar[1] . " AND " . $bar[2];
+    } else {
+        $rule = $bar[2];
+    }
+
+    $rule = str_replace("OR", "/", $rule);
+    //explode rule
+    $exRule = explode(" AND ", $rule);
+    $jml_ExRule = count($exRule);
+    $jml_temp = count($temp_rule);
+
+    $i = 0;
+    while ($i < $jml_ExRule) {
+        if ($temp_rule[$i] == $exRule[$i]) {
+            $temp_rule[$i] = $exRule[$i];
+            $exRule[$i] = "---- ";
+        } else {
+            $temp_rule[$i] = $exRule[$i];
+        }
+
+        if ($i == ($jml_ExRule - 1)) {
+            $t = $i;
+            while ($t < $jml_temp) {
+                $temp_rule[$t] = "";
+                $t++;
+            }
+        }
+
+        //jika terakhir tambah cetak keputusan
+        if ($i == ($jml_ExRule - 1)) {
+            $strip = '';
+            for ($x = 1; $x <= $i; $x++) {
+                $strip = $strip . "---- ";
+            }
+            $sql_que = $db_object->db_query("SELECT keputusan FROM t_keputusan WHERE id=$id[$ll]");
+            $row_bar = $db_object->db_fetch_array($sql_que);
+            if ($exRule[$i - 1] == "---- ") {
+                echo "<font color='#000'><b>" . $exRule[$i] . "</b></font> <i>Maka donor darah = </i><strong>" . $row_bar[0] . " (" . $id[$ll] . ")</strong>";
+            } else if ($exRule[$i - 1] != "---- ") {
+                echo "<br>" . $strip . "<font color='#000'><b>" . $exRule[$i] . "</b></font> <i>Maka donor darah = </i><strong>" . $row_bar[0] . "  (" . $id[$ll] . ")</strong>";
+            }
+        }
+        //jika pertama
+        else if ($i == 0) {
+            if ($ll == 1) {
+                echo "<font color='#000'><b>" . $exRule[$i] . "</b></font> <b></b>";
+            } else {
+                echo $exRule[$i] . " ";
+            }
+        }
+        //jika ditengah
+        else {
+            if ($exRule[$i] == "---- ") {
+                echo $exRule[$i] . " ";
+            } else {
+                if ($exRule[$i - 1] == "---- ") {
+                    echo "<font color='#000'><b>" . $exRule[$i] . "</b></font> <b></b>";
+                } else {
+                    $strip = '';
+                    for ($x = 1; $x <= $i; $x++) {
+                        $strip = $strip . "---- ";
+                    }
+                    echo "<br>" . $strip . "<font color='#000'><b>" . $exRule[$i] . "</b></font> <b></b>";
+                }
+            }
+        }
+        $i++;
+    }
+    echo "<br>";
+    $ll++;
+}
+
+ * 
+ */
+?>
+
+        </div>
+    </div>
+</div>
