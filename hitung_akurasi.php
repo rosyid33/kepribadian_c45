b817827277593136ad8ba362d56de03f725b4067
rosyid_ido <rosyid33@gmail.com>
2017-05-22 07:14:24 +0700

    fix hitung akurasi

--- hitung_akurasi.php
+++ hitung_akurasi.php
@@ -205,51 +205,113 @@ include_once "fungsi.php";
             <?php
 //perhitungan akurasi
             $que = $db_object->db_query("SELECT * FROM data_uji");
-            $jumlah = $db_object->db_num_rows($que);
-            $TP = 0;
-            $FN = 0;
-            $TN = 0;
-            $FP = 0;
+            $jumlah_uji=$db_object->db_num_rows($que);
+            //$TP=0; $FN=0; $TN=0; $FP=0; $kosong=0;
+            $TA = $FB = $FC = $FD = 
+            $FE = $TF = $FG = $FH = 
+            $FI = $FJ = $TK = $FL = 
+            $FM = $FN = $FO = $TP = 0;
             $kosong = 0;
             while ($row = $db_object->db_fetch_array($que)) {
                 $asli = $row['kelas_asli'];
                 $prediksi = $row['kelas_hasil'];
-                if (($asli == 'baik' || $asli == 'Baik') & ($prediksi == 'baik') || $prediksi == 'Baik') {
-                    $TP++;
-                } else if (($asli == 'baik' || $asli == 'Baik') & ($prediksi == 'kurang' || $prediksi == 'Kurang')) {
-                    $FN++;
-                } else if (($asli == 'kurang' || $asli == 'Kurang') & ($prediksi == 'kurang') || $prediksi == 'Kurang') {
-                    $TN++;
-                } else if (($asli == 'kurang' || $asli == 'Kurang') & ($prediksi == 'baik') || $prediksi == 'Baik') {
-                    $FP++;
-                } else if ($prediksi == '') {
-                    $kosong++;
+                if($asli=='Sanguin' & $prediksi=='Sanguin'){
+                        $TA++;
+                }
+                else if($asli=='Sanguin' & $prediksi=='Koleris'){
+                        $FB++;
+                }
+                else if($asli=='Sanguin' & $prediksi=='Melankolis'){
+                        $FC++;
+                }
+                else if($asli=='Sanguin' & $prediksi=='Plegmatis'){
+                        $FD++;
+                }
+                else if($asli=='Koleris' & $prediksi=='Sanguin'){
+                        $FE++;
+                }
+                else if($asli=='Koleris' & $prediksi=='Koleris'){
+                        $TF++;
+                }
+                else if($asli=='Koleris' & $prediksi=='Melankolis'){
+                        $FG++;
+                }
+                else if($asli=='Koleris' & $prediksi=='Plegmatis'){
+                        $FH++;
+                }
+                else if($asli=='Melankolis' & $prediksi=='Sanguin'){
+                        $FI++;
+                }
+                else if($asli=='Melankolis' & $prediksi=='Koleris'){
+                        $FJ++;
+                }
+                else if($asli=='Melankolis' & $prediksi=='Melankolis'){
+                        $TK++;
+                }
+                else if($asli=='Melankolis' & $prediksi=='Plegmatis'){
+                        $FL++;
+                }
+                else if($asli=='Plegmatis' & $prediksi=='Sanguin'){
+                        $FM++;
+                }
+                else if($asli=='Plegmatis' & $prediksi=='Koleris'){
+                        $FN++;
+                }
+                else if($asli=='Plegmatis' & $prediksi=='Melankolis'){
+                        $FO++;
+                }
+                else if($asli=='Plegmatis' & $prediksi=='Plegmatis'){
+                        $TP++;
+                }
+                else if($prediksi==''){
+                        $kosong++;
                 }
             }
-            $tepat = ($TP + $TN);
-            $tidak_tepat = ($FP + $FN + $kosong);
-            $akurasi = ($tepat / $jumlah) * 100;
-            $laju_error = ($tidak_tepat / $jumlah) * 100;
-            $sensitivitas = ($TP / ($TP + $FN)) * 100;
-            $spesifisitas = ($TN / ($FP + $TN)) * 100;
-
-            $akurasi = round($akurasi, 2);
-            $laju_error = round($laju_error, 2);
-            $sensitivitas = round($sensitivitas, 2);
-            $spesifisitas = round($spesifisitas, 2);
-//echo "<center><h4>";
-            echo "Jumlah data: $jumlah<br>";
-            echo "Jumlah data yang tepat: $tepat<br>";
-            echo "Jumlah data yang tidak tepat: $tidak_tepat<br>";
-            if ($kosong != 0) {
-                echo "Jumlah data yang kosong: $kosong<br></h4>";
-            }
-            echo "<h2>AKURASI = $akurasi %<br>";
-            echo "LAJU ERROR = $laju_error %<br></h2>";
-            echo "<h4>TP: $TP | TN: $TN | FP: $FP | FN: $FN<br></h4>";
-            echo "<h2>SENSITIVITAS = $sensitivitas %<br>";
-            echo "SPESIFISITAS = $spesifisitas %<br>";
+//            $tepat = ($TP + $TN);
+//            $tidak_tepat = ($FP + $FN + $kosong);
+//            $akurasi = ($tepat / $jumlah) * 100;
+//            $laju_error = ($tidak_tepat / $jumlah) * 100;
+//            $sensitivitas = ($TP / ($TP + $FN)) * 100;
+//            $spesifisitas = ($TN / ($FP + $TN)) * 100;
+//
+//            $akurasi = round($akurasi, 2);
+//            $laju_error = round($laju_error, 2);
+//            $sensitivitas = round($sensitivitas, 2);
+//            $spesifisitas = round($spesifisitas, 2);
+////echo "<center><h4>";
+//            echo "Jumlah data: $jumlah<br>";
+//            echo "Jumlah data yang tepat: $tepat<br>";
+//            echo "Jumlah data yang tidak tepat: $tidak_tepat<br>";
+//            if ($kosong != 0) {
+//                echo "Jumlah data yang kosong: $kosong<br></h4>";
+//            }
+//            echo "<h2>AKURASI = $akurasi %<br>";
+//            echo "LAJU ERROR = $laju_error %<br></h2>";
+//            echo "<h4>TP: $TP | TN: $TN | FP: $FP | FN: $FN<br></h4>";
+//            echo "<h2>SENSITIVITAS = $sensitivitas %<br>";
+//            echo "SPESIFISITAS = $spesifisitas %<br>";
 //echo "</h2></center>";
+            $tepat=($TA+$TF+$TK+$TP);
+                    $tidak_tepat=($FB+$FC+$FD+$FE+$FG+$FH+$FI+$FJ+$FL+$FM+$FN+$FO+$kosong);
+                    $akurasi=($tepat/$jumlah_uji)*100;
+                    $laju_error=($tidak_tepat/$jumlah_uji)*100;
+//                        $sensitivitas=($TP/($TP+$FN))*100;
+//                        $spesifisitas=($TN/($FP+$TN))*100;
+
+                    $akurasi = round($akurasi,2);	
+                    $laju_error = round($laju_error,2);
+                    $sensitivitas = round($sensitivitas,2);	
+                    $spesifisitas = round($spesifisitas,2);	
+
+
+                    echo "<br><br>";
+                    echo "<center><h4>";
+                    echo "Jumlah prediksi: $jumlah_uji<br>";
+                    echo "Jumlah tepat: $tepat<br>";
+                    echo "Jumlah tidak tepat: $tidak_tepat<br>";
+                    if($kosong!=0){ echo "Jumlah data yang prediksinya kosong: $kosong<br></h4>"; }
+                    echo "<h2>AKURASI = $akurasi %<br>";
+                    echo "LAJU ERROR = $laju_error %<br></h2>";
             ?>
         </div>
     </div>
